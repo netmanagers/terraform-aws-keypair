50cb059f80518c7b876eb9a67f71b70407538ac7
Javier Bértoli <javier@netmanagers.com.ar>
2020-04-30 16:20:49 -0300

    feat(outputs): add ARN of the key

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "name" {
   value       = join("", aws_key_pair.default.*.key_name)
   description = "Name of SSH key."
 }
+output "arn" {
+  value       = join("", aws_key_pair.default.*.arn)
+  description = "The Amazon Resource Name (ARN) of the key."
+}
